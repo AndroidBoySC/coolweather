192e9e23f529cc2ee9f59721eaf6739dc5ec69a3
songchao <1620674599@qq.com>
2017-07-07 11:42:30 +0800

    First commit.

---
+++ app/src/main/java/com/example/cf/coolweather/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.cf.coolweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 493c0ce3d5ba4d519387e593e1580ab7
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
